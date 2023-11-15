70f3fc950e5cdcff15d3eb94068cf7b20f3557ba
Sangmin <101931428+netco97@users.noreply.github.com>
2023-11-15 16:56:26 +0900

    commit test

---
+++ src/main/java/com/example/react_spring/ReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.react_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/react_spring/ReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
